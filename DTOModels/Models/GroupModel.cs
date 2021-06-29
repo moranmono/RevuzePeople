9dffb4ab86246ba05988ce325db331513ac43a62
Moran Monovich <moranm22@gmail.com>
2021-06-29 17:33:50 +0300

    Adding user and group model

---
+++ DTOModels/Models/GroupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOModels.Models
+{
+    public class GroupModel
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public List<UserModel> UserModels { get; set; }
+    }
+}
